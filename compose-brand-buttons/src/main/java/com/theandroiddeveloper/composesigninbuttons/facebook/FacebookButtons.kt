ca508f5dfdd27230767c3a17b2d6d1addbb54fcf
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-11-18 13:16:42 +0530

    refactor: add DarkFacebookButton

--- compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/facebook/FacebookButtons.kt
+++ compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/facebook/FacebookButtons.kt
@@ -1,2 +1,37 @@
 package com.theandroiddeveloper.composesigninbuttons.facebook
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonElevation
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import com.theandroiddeveloper.composesigninbuttons.BaseBrandButton
+import com.theandroiddeveloper.composesigninbuttons.BrandedButtonType
+
+@Composable
+fun DarkFacebookButton(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = LocalTextStyle.current,
+    enabled: Boolean = true,
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null
+) {
+    BaseBrandButton(
+        brandedButtonType = BrandedButtonType.DarkFacebookButton,
+        label = label,
+        textStyle = textStyle,
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        elevation = elevation,
+        shape = shape,
+        border = border
+    )
+}
