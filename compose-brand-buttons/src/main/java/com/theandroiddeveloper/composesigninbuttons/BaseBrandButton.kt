721d6ac1025a9dbcda654071e6cb3c6c6996c65f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-11-19 20:49:19 +0530

    refactor: remove BrandButton overload that uses BrandedButtonTypeParameter This commit also removes all composables that used the BrandButton overload with BrandedButtonType parameter.

--- compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/BaseBrandButton.kt
+++ compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/BaseBrandButton.kt
@@ -18,63 +18,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 /**
- * A button that contains a brand icon followed by [label]. It sets the appearance of the
- * button based on the properties defined in [brandedButtonType].
- * @param brandedButtonType the type of branded button to display.
- * @param label the text that will appear after the brand icon.
- * @param textStyle the [TextStyle] to be applied for the [label].
- * @param onClick the lambda to execute when the button is clicked.
- * @param modifier the modifier to be applied to the button.
- * @param enabled Controls the enabled state of the button. When `false`, this button will not
- * be clickable.
- * @param elevation [ButtonElevation] used to resolve the elevation for this button in different
- * @param shape Defines the button's shape as well as its shadow.
- * @param border Border to draw around the button.
- */
-@Composable
-internal fun BrandButton(
-    brandedButtonType: BrandedButtonType,
-    label: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    textStyle: TextStyle = LocalTextStyle.current,
-    enabled: Boolean = true,
-    elevation: ButtonElevation? = ButtonDefaults.elevation(),
-    shape: Shape = MaterialTheme.shapes.small,
-    border: BorderStroke? = null
-) {
-    Button(
-        modifier = Modifier
-            .sizeIn(minHeight = 40.dp)
-            .then(modifier),
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(backgroundColor = brandedButtonType.backgroundColor),
-        enabled = enabled,
-        elevation = elevation,
-        shape = shape,
-        border = border,
-        contentPadding = brandedButtonType.contentPadding
-    ) {
-        Row(
-            modifier = Modifier.semantics(mergeDescendants = true) {
-                // TODO test and document
-                role = Role.Button
-                contentDescription = label
-            },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier.height(brandedButtonType.iconHeight),
-                imageVector = ImageVector.vectorResource(brandedButtonType.iconResId),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.width(24.dp))
-            Text(text = label, style = textStyle, color = brandedButtonType.contentColor)
-        }
-    }
-}
-
-/**
  * A button that contains a brand icon followed by the [label]. It sets the appearance of the
  * button based on the properties defined in [brand].
  * @param brand defines the brand of the button based on which the appearance will be set.
