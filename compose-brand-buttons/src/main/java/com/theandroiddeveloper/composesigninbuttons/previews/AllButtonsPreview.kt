91d1d7a68502aec072e5a8902aed90d8f921c88e
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-11-19 20:26:20 +0530

    refactor: add apple and facebook buttons to AllButtonsPreview

--- compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/previews/AllButtonsPreview.kt
+++ compose-brand-buttons/src/main/java/com/theandroiddeveloper/composesigninbuttons/previews/AllButtonsPreview.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import com.theandroiddeveloper.composesigninbuttons.apple.DarkAppleButton
+import com.theandroiddeveloper.composesigninbuttons.apple.LightAppleButton
+import com.theandroiddeveloper.composesigninbuttons.facebook.DarkFacebookButton
 import com.theandroiddeveloper.composesigninbuttons.google.DarkGoogleButton
 import com.theandroiddeveloper.composesigninbuttons.google.LightGoogleButton
 import com.theandroiddeveloper.composesigninbuttons.twitter.DarkTwitterButton
@@ -18,6 +21,9 @@ internal fun AllButtonsPreview() {
             LightGoogleButton(label = "Sign in with Google", onClick = { /*TODO*/ })
             DarkTwitterButton(label = "Sign in with Twitter", onClick = { /*TODO*/ })
             LightTwitterButton(label = "Sign in with Twitter", onClick = { /*TODO*/ })
+            DarkFacebookButton(label = "Sign in with Facebook", onClick = { /*TODO*/ })
+            DarkAppleButton(label = "Sign in with Apple", onClick = { /*TODO*/ })
+            LightAppleButton(label = "Sign in with Apple", onClick = { /*TODO*/ })
         }
     }
 }
